2618307b9ed934621eff7bf47cc5b61fb069ae57
stryear <1044689557@qq.com>
2026-03-01 02:40:55 +0800

    改为仅使用本地JetBot资产进行训练
    Signed-off-by: stryear <1044689557@qq.com>

--- source/isaac_lab_tutorial/isaac_lab_tutorial/robots/jetbot.py
+++ source/isaac_lab_tutorial/isaac_lab_tutorial/robots/jetbot.py
@@ -3,13 +3,10 @@ from pathlib import Path
 import isaaclab.sim as sim_utils
 from isaaclab.actuators import ImplicitActuatorCfg
 from isaaclab.assets import ArticulationCfg
-from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 
 _JETBOT_USD = Path(__file__).resolve().parents[1] / "assets" / "Jetbot" / "jetbot.usd"
-_JETBOT_NUCLEUS_USD = f"{ISAAC_NUCLEUS_DIR}/Robots/NVIDIA/Jetbot/jetbot.usd"
-_JETBOT_USD_PATH = str(_JETBOT_USD) if _JETBOT_USD.exists() else _JETBOT_NUCLEUS_USD
 
 JETBOT_CONFIG = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(usd_path=_JETBOT_USD_PATH),
+    spawn=sim_utils.UsdFileCfg(usd_path=str(_JETBOT_USD)),
     actuators={"wheel_acts": ImplicitActuatorCfg(joint_names_expr=[".*"], damping=None, stiffness=None)},
 )
